a98a7d6d54623a99b086bb6ab2dd2ad9fe8dca79
markOone <98772113+markOone@users.noreply.github.com>
2024-11-11 12:42:16 +0200

    Task 1

---
+++ Lab4/Task1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function sum(...args)
+{
+    let result = 0;
+    for(let i = 0; i < args.length; i++)
+    {
+        result += args[i];
+    }
+
+    return result;
+}
+
+console.log(sum(1, 2, 3));
