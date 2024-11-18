79edb48811274bf37bd31e385855d9e078c02131
markOone <98772113+markOone@users.noreply.github.com>
2024-11-18 14:02:25 +0200

    Task 2

---
+++ Lab5/Task2.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function array(){
+    let arr = [];
+
+    return{
+        push(value){
+            arr.push(value);
+        },
+        pop(){
+            return arr.pop();
+        },
+        get(value){
+            return arr[value];
+        }
+    }
+}
+
+const arr = array();
+
+arr.push("first");
+
+console.log(arr.get(0));
